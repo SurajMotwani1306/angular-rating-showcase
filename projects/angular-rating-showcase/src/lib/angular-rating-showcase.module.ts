94c3c6a9fbca21bbd2d6b131930a9aab83bef49e
SurajMotwani1306 <suraj.motwani1306@gmail.com>
2024-07-13 04:21:20 -0700

    Another Approach Followed

--- projects/angular-rating-showcase/src/lib/angular-rating-showcase.module.ts
+++ projects/angular-rating-showcase/src/lib/angular-rating-showcase.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { AngularRatingShowcaseComponent } from './angular-rating-showcase.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { YellowComponent } from './components/yellow/yellow.component';
 
 @NgModule({
   declarations: [
-    AngularRatingShowcaseComponent
+    AngularRatingShowcaseComponent,
+    YellowComponent
   ],
   imports: [
     BrowserModule,
